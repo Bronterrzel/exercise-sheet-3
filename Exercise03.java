175f43537e51de255d2d838f3a4590646a931f41
Tim Schmidt <tim@amoto.de>
2018-11-05 11:05:42 +0100

    Consolifiziert

--- Exercise03.java
+++ Exercise03.java
@@ -1,4 +1,4 @@
-/**
+;/**
  * Beschreiben Sie hier die Klasse Exercise03.
  * 
  * @author (Ihr Name) 
@@ -18,10 +18,19 @@ public class Exercise03 extends Exercise03Base
         paule.write("Es existieren " + territory.getTotalGrainCount() + " Koerner.");
         
         //Console output
-        paule.write("Hier sind " + territory.getTotalHamsterCount() + " Hamster.");
-        paule.write("Das Territorium ist " + territory.getTerritorySize().getColumnCount() 
-        + " auf " + territory.getTerritorySize().getRowCount() + " Felder groß.");
-        paule.write("Es existieren " + territory.getTotalGrainCount() + " Koerner.");
+        System.out.print("Hier sind ");
+        System.out.print(territory.getTotalHamsterCount());
+        System.out.println(" Hamster.");
+        
+        System.out.print("Das Territorium ist ");
+        System.out.print(territory.getTerritorySize().getColumnCount()); 
+        System.out.print(" auf ");
+        System.out.print(territory.getTerritorySize().getRowCount());
+        System.out.println(" Felder groß.");
+        
+        System.out.print("Es existieren ");
+        System.out.print(territory.getTotalGrainCount());
+        System.out.println(" Koerner.");
         
         
         // Leave this call intact for challenge 3!
